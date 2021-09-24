f27dd8b8809ff4d8998e6d1963df589d7e588dfa
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-09-24 15:14:03 +0100

    function block layout

--- week-3/activity16-functions/script.js
+++ week-3/activity16-functions/script.js
@@ -1,1 +1,22 @@
 // Write Your Code Below
+const equality = function (val1, val2) {
+    if () {
+
+    } else if () {
+
+    } else {
+        
+    }
+};
+
+
+// Call function: equality (val1, val2)
+
+// === Strictly equal
+equality (2, 2)
+
+// == Equal in value
+equality (4, "4")
+
+// !== Not equal
+equality (4, -8)
