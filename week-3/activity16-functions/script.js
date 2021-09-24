870bbb385fda33de6e6c73e57a606d42dabb2233
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-09-24 15:34:47 +0100

    completed function return statements

--- week-3/activity16-functions/script.js
+++ week-3/activity16-functions/script.js
@@ -1,22 +1,22 @@
 // Write Your Code Below
+// Function declaration
 const equality = function (val1, val2) {
-    if () {
-
-    } else if () {
-
-    } else {
-        
-    }
+  if (val1 === val2) {
+    console.log("They are equal in type and value");
+  } else if (val1 == val2) {
+    console.log("They are equal in value");
+  } else {
+    console.log("The values are not equal");
+  }
 };
 
-
-// Call function: equality (val1, val2)
+// Tests:
 
 // === Strictly equal
-equality (2, 2)
+equality(2, 2);
 
 // == Equal in value
-equality (4, "4")
+equality(4, "4");
 
 // !== Not equal
-equality (4, -8)
+equality(4, -8);
