28691844bb517d758ae912547b0da17f1efabe91
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-09-25 23:25:23 +0100

    pseudo code

---
+++ week-3/rock-paper-scissors/assets/js/script.js
@@ -0,0 +1,24 @@
+// prompt user for input value (r, s, or p)
+
+// getUserValue
+// validate userValue
+// if true
+// convert to lowercase
+// store userValue
+// if false
+// alert "Please enter a valid response using 'r,' 's,' or 'p.'"
+
+// getComputerValue
+// choose a random value from array of [r, s, p]
+// store computerValue
+
+// gameLogic
+// compare userValue and computerValue
+// outputs: win, lose, or tie
+
+// displayScore
+// calculate score using increment ++
+
+// confirm if user wants to play again
+// Y - repeat from getUserValue
+// N - end
