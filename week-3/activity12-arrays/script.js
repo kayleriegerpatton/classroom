112b5ee0de11bef2efaaaf8bcab3ecc2d4e819e4
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-09-25 22:29:24 +0100

    inital commit

--- week-3/activity12-arrays/script.js
+++ week-3/activity12-arrays/script.js
@@ -1,5 +1,16 @@
 // WRITE YOUR CODE HERE
+// create array and assign values
 const studentNames = ["Andrew", "Blake", "Carl", "Dan"];
 
-// Tests
+// Welcome message
+
+
+// reassign an array element
+studentNames[3] = "Danielle";
+
+// Test logs
+// displays all names
 console.log(studentNames);
+
+// displays 4
+console.log(studentNames.length);
