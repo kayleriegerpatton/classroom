c034ede791f56d61123219e99cbc4c566f56462f
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-10-01 13:25:43 +0100

    added preventDefault method

--- week-4/14-Stu_Preventing-Default/Solved/assets/js/script.js
+++ week-4/14-Stu_Preventing-Default/Solved/assets/js/script.js
@@ -1,6 +1,6 @@
-var tipEl = document.querySelector('#tip-percentage');
-var totalEl = document.querySelector('#total');
-var submitEl = document.querySelector('#submit');
+var tipEl = document.querySelector("#tip-percentage");
+var totalEl = document.querySelector("#total");
+var submitEl = document.querySelector("#submit");
 
 function calculateTip(total, tipPercentage) {
   var roundedResult = (total * tipPercentage).toFixed(2);
@@ -12,12 +12,13 @@ function calculateTotal(total, tipAmount) {
 }
 
 function addTip(event) {
+  event.preventDefault();
   var tipPercentage = tipEl.value * 0.01;
   var total = totalEl.value;
   var tipAmount = calculateTip(total, tipPercentage);
   var newTotal = calculateTotal(tipAmount, total);
-  document.querySelector('#tip-amount').textContent = tipAmount;
-  document.querySelector('#new-total').textContent = newTotal.toFixed(2);
+  document.querySelector("#tip-amount").textContent = tipAmount;
+  document.querySelector("#new-total").textContent = newTotal.toFixed(2);
 }
 
-submitEl.addEventListener('click', addTip);
+submitEl.addEventListener("click", addTip);
