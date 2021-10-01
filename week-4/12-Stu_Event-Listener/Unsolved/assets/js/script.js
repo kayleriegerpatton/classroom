acee00a1ecfa571f358685509824d5f4af3bdf84
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-10-01 10:18:53 +0100

    added eventlisteners

--- week-4/12-Stu_Event-Listener/Unsolved/assets/js/script.js
+++ week-4/12-Stu_Event-Listener/Unsolved/assets/js/script.js
@@ -1,12 +1,21 @@
 var count = 0;
-var incrementEl = document.querySelector('#increment');
-var decrementEl = document.querySelector('#decrement');
-var countEl = document.querySelector('#count');
 
+// target increment button
+var incrementEl = document.querySelector("#increment");
+
+// target decrement button
+var decrementEl = document.querySelector("#decrement");
+
+// target count text element
+var countEl = document.querySelector("#count");
+
+// TODO: Add event listener to increment button
+incrementEl.addEventListener("click", setCounterText);
+
+// TODO: Add event listener to decrement button
+decrementEl.addEventListener("click", setCounterText);
+
+// declare counter function
 function setCounterText() {
   countEl.textContent = count;
 }
-
-// TODO: Add event listener to increment button
-
-// TODO: Add event listener to decrement button
