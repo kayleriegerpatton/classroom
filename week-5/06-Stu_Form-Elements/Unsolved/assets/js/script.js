1e80869b711063fb00e06628c32bd14c67f38660
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-10-05 22:55:54 +0100

    added preventDefault

--- week-5/06-Stu_Form-Elements/Unsolved/assets/js/script.js
+++ week-5/06-Stu_Form-Elements/Unsolved/assets/js/script.js
@@ -1,6 +1,13 @@
-var shoppingFormEl = $('#shopping-form');
-var shoppingListEl = $('#shopping-list');
+var shoppingFormEl = $("#shopping-form");
+var shoppingListEl = $("#shopping-list");
 
 // TODO: Create a function to handle the form submission event that captures the form's `<input>` value and prints it to the `shoppingListEl` as a `<li>`
 
 // TODO: Add an event listener to the `shoppingFormEl` to handle submission
+
+const handleSubmit = function (event) {
+  event.preventDefault();
+  //   console.log("click");
+};
+
+shoppingFormEl.on("submit", handleSubmit);
