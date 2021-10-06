59ed171285697e9a0244b5c035f38764b01bb52a
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-10-06 12:07:10 +0100

    targeted DOM elements, set css style

--- week-5/08-Stu_Dom-Traversal/Unsolved/assets/js/script.js
+++ week-5/08-Stu_Dom-Traversal/Unsolved/assets/js/script.js
@@ -1,8 +1,28 @@
 // Assemble: Create/select DOM elements
-var rootEl = $('#root');
+var rootEl = $("#root");
+
+const title = $(".text-center").text("Tic Tac Toe");
+
+const boxOne = $("#item-a1");
+const boxTwo = $("#item-b1");
+const boxThree = $("item-c1").text("X");
+const rowOne = $("#row1").append(boxOne, boxTwo, boxThree);
+
+const boxFour = $("#item-a2").text("O");
+const boxFive = $("#item-b2").text("X");
+const boxSix = $("item-c2");
+const rowTwo = $("#row2").append(boxFour, boxFive, boxSix);
+
+const boxSeven = $("#item-a3");
+const boxEight = $("#item-b3");
+const boxNine = $("item-c3");
+const rowThree = $("#row3").append(boxSeven, boxEight, boxNine);
+
+rootEl.append(title, rowOne, rowTwo, rowThree);
 
 // TODO: Starting from the root element, select all the boxes and turn them white in a single statement.
 // HINT: Use the `.css` function to change element's background color
+$("li").css({ "background-color": "#fff" });
 
 // TODO: Starting from `rootEl`, what is the statement that will use "O" to block the "X" from winning
 // HINT: Use the `.closest()`, or `.parent()` methods to go up the DOM tree
