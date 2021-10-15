5995ed82ac2f8b5d8c9d1c905a4238b159c0f229
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-10-15 14:39:23 +0100

    5 per page response

--- week-6/08-Stu_Parse_JSON/Unsolved/assets/js/script.js
+++ week-6/08-Stu_Parse_JSON/Unsolved/assets/js/script.js
@@ -1,11 +1,12 @@
 // TODO: Edit the URL to get only 5 issues of Twitter's Chill repo
-var requestUrl = "https://api.github.com/repos/twitter/chill/issues";
+var requestUrl = "https://api.github.com/repos/twitter/chill/issues?per_page=5";
 
 fetch(requestUrl)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
+    console.log(data);
     console.log("Github Repo Issues \n----------");
     const showData = function (each) {
       console.log(each.url);
