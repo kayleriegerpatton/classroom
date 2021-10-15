edf29e9165abff711122cbb06c46678918a6dfb9
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-10-15 14:34:23 +0100

    logged data, need to limit responses

--- week-6/08-Stu_Parse_JSON/Unsolved/assets/js/script.js
+++ week-6/08-Stu_Parse_JSON/Unsolved/assets/js/script.js
@@ -1,13 +1,18 @@
 // TODO: Edit the URL to get only 5 issues of Twitter's Chill repo
-var requestUrl = 'https://api.github.com/repos/{owner}/{repo}/issues';
+var requestUrl = "https://api.github.com/repos/twitter/chill/issues";
 
 fetch(requestUrl)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    console.log('Github Repo Issues \n----------');
-    console.log(data);
+    console.log("Github Repo Issues \n----------");
+    const showData = function (each) {
+      console.log(each.url);
+      console.log(each.user.login);
+    };
+
     // TODO: Loop through the response
+    data.forEach(showData);
     // TODO: Console log each issue's URL and each user's login
   });
