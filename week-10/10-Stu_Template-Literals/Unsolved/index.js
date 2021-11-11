8ecb69d5553dd0ebb93a65e6573a61e872d7ec52
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-11-11 12:33:55 -0500

    solved activity 10

--- week-10/10-Stu_Template-Literals/Unsolved/index.js
+++ week-10/10-Stu_Template-Literals/Unsolved/index.js
@@ -1,11 +1,12 @@
 // music should be an object with title, artist, and album properties
 const music = {
   // code here
- 
+  title: "All You Need is Love",
+  artist: "The Beatles",
+  album: "Yellow Submarine",
 };
 
 // Write code between the backticks tags to output the data from the music object above.
-const songSnippet = ``;
+const songSnippet = `${music.title} by ${music.artist} is part of their album ${music.album}.`;
 
-console.log(songSnippet)
-
+console.log(songSnippet);
