f38de23632f4c22a807add1eb21aa6b2f573e613
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-11-17 16:09:39 -0600

    complete activity 02

---
+++ week-11/02-stu-constructors/index.js
@@ -0,0 +1,18 @@
+// TODO: Create a constructor function called 'Developer' that takes in 'name' and 'tech'
+class Developer {
+  constructor(name, tech) {
+    this.name = name;
+    this.tech = tech;
+  }
+  // TODO: Include a method called 'introduction()' that introduces the Developer with their name and favorite tech
+  //   introduce developer
+  introduction() {
+    return `Hi, I\'m ${this.name} and I love ${this.tech}.`;
+  }
+}
+
+// TODO: Create a new object using the 'Developer' constructor
+const kayle = new Developer("Kayle", "CSS");
+
+// TODO: Call the 'introduction()' method on the new object
+console.log(kayle.introduction());
