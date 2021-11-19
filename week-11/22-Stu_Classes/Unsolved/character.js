2124a6ed60bcbb3aedbd49096c88abe1d58adbe1
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-11-18 20:32:02 -0600

    subclasses

--- week-11/22-Stu_Classes/Unsolved/character.js
+++ week-11/22-Stu_Classes/Unsolved/character.js
@@ -1,25 +1,32 @@
 class Character {
   // TODO: Add a constructor
-  constructor (name, strength, hitpoints){
-    this.name = name,
-    this.strength = strength,
-    this.hitpoints =hitpoints,
+  constructor(name, strength, hitpoints) {
+    (this.name = name),
+      (this.strength = strength),
+      (this.hitpoints = hitpoints);
   }
+
   // TODO: Create a printStats() method that console logs `this.name`, `this.strength`, and `this.hitPoints`
-  printStats(){
-    console.log(`Stats for ${this.name} are:\n`)
-    console.log(`Each attack will do ${this.strength} damage.`)
-    console.log(`${this.name} has ${this.hitpoints} hit points remaining.`)
+  printStats() {
+    console.log(`Stats for ${this.name} are:\n`);
+    console.log(`Each attack will do ${this.strength} damage.`);
+    console.log(`${this.name} has ${this.hitpoints} hit points remaining.`);
   }
 
   // TODO: Create a isAlive() method that returns a boolean based on whether or not a character's "hitpoints" are <= 0
+  isAlive() {
+    this.hitpoints <= 0 ? true : false;
+  }
 
   // TODO: Create a attack() method that accepts an opponent object and decreases the opponent's "hitPoints" by this character's strength
+  attack(character2) {
+    character2.hitpoints -= this.strength;
+  }
 }
 
 // Creates two unique characters using the "character" constructor
-const grace = new Character('Grace', 30, 75);
-const dijkstra = new Character('Dijkstra', 20, 105);
+const grace = new Character("Grace", 30, 75);
+const dijkstra = new Character("Dijkstra", 20, 105);
 
 // This keeps track of whose turn it is
 let graceTurn = true;
@@ -31,7 +38,7 @@ const turnInterval = setInterval(() => {
   // If either character is not alive, end the game
   if (!grace.isAlive() || !dijkstra.isAlive()) {
     clearInterval(turnInterval);
-    console.log('Game over!');
+    console.log("Game over!");
   } else if (graceTurn) {
     grace.attack(dijkstra);
     dijkstra.printStats();
--- week-11/24-Stu_Subclasses/Unsolved/boat.js
+++ week-11/24-Stu_Subclasses/Unsolved/boat.js
@@ -1,17 +1,31 @@
 // TODO: Import the parent class
+const Vehicle = require("./vehicle.js");
 
 // TODO: Create a `Boat` class that extends the `Vehicle` class
+class Boat extends Vehicle() {
+  constructor(type, crew) {
+    super(id, numberOfWheels, sound);
+    this.numberOfWheels = 0;
+    this.sound = "bwom";
+    this.type = type;
+    this.crew = crew;
+  }
+  useHorn() {
+    console.log(`${this.sound}`);
+  }
+  crewSoundOff() {}
+}
 
 const boatPassengers = [
-  'Blackbeard',
-  'Mary Read',
-  'Henry Morgan',
-  'Madame Cheng',
+  "Blackbeard",
+  "Mary Read",
+  "Henry Morgan",
+  "Madame Cheng",
 ];
 
-const boat = new Boat(16, 'sailboat', boatPassengers);
+const boat = new Boat(16, "sailboat", boatPassengers);
 
-console.log('---BOAT---');
+console.log("---BOAT---");
 boat.printInfo();
 boat.useHorn();
 boat.crewSoundOff();
