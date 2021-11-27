ec7f784ed5ae34adf6b5e5e0b914ddc8e6261460
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-11-26 20:26:23 -0600

    solve activity 4

--- week-12/04-Stu_API-HTML-Routes/Unsolved/server.js
+++ week-12/04-Stu_API-HTML-Routes/Unsolved/server.js
@@ -1,11 +1,22 @@
 // TODO: Import express
+const express = require("express");
+const path = require("path");
 
 // TODO: Import 'terms.json' file
-const PORT = 3001;
+const terms = require("./terms.json");
+
+const PORT = 3002;
 
 // TODO: Initialize app variable
+const app = express();
 
 // TODO: Create a route for a GET request that will return the content of our `terms.json` file
+app.get("/api/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./terms.json"));
+  // res.json({ message: "hello world" });
+});
+
+console.log(path.join(__dirname, "./terms.json"));
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
