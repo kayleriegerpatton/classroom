e6ec56400f08dee5d5e1454bd6a76b24b2557bab
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-12-03 21:12:32 +0000

    day 1 activities

--- week-13/04-Stu_Databases/Unsolved/schema.sql
+++ week-13/04-Stu_Databases/Unsolved/schema.sql
@@ -1,1 +1,4 @@
 -- Write code here or enter directly in MySQL shell --
+DROP DATABASE IF EXISTS sample_db;
+CREATE DATABASE sample_db;
+USE sample_db;
--- week-13/08-Stu_CRUD-Insert/Unsolved/db/insert-select.sql
+++ week-13/08-Stu_CRUD-Insert/Unsolved/db/insert-select.sql
@@ -1,7 +1,9 @@
 INSERT INTO biographies (id, name)
+-- Insert three new records (rows) with the following ids and names
 VALUES
     ( 001, "Diary of Anne Frank"),
     ( 002, "Frida: A Biography of Frida Kahlo"),
     ( 003, "Long Walk to Freedom");
 
+-- display all data in the biographies table
 SELECT * FROM biographies;
