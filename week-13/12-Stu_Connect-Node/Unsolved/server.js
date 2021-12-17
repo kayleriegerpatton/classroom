b5a55966c40d83612ceaf45ae29673176c0e5eb6
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-12-17 16:39:02 +0000

    week 15 activities

--- week-13/12-Stu_Connect-Node/Unsolved/server.js
+++ week-13/12-Stu_Connect-Node/Unsolved/server.js
@@ -1,30 +1,37 @@
-const express = require('express');
-const mysql = require('mysql2');
+// import express and mySQL2 packages
+const express = require("express");
+const mysql = require("mysql2");
 
+// set up port and express app
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// connect to database
 const db = mysql.createConnection(
   {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'classlist_db'
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "classlist_db",
   },
   console.log(`Connected to the classlist_db database.`)
 );
 
-db.query('SELECT * FROM students', function (err, results) {
+// query db database
+db.query("SELECT * FROM students", function (err, results) {
   console.log(results);
 });
 
+// set up failed response operation
 app.use((req, res) => {
   res.status(404).end();
 });
 
+// connect port
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
