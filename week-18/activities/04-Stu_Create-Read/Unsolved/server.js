a7ffc02b9062ce0f7a71b1abd10f5e261a70d30e
Kayle Rieger Patton <kayle.patton22@gmail.com>
2022-02-05 19:31:26 +0000

    finished activity 18

--- week-18/activities/04-Stu_Create-Read/Unsolved/server.js
+++ week-18/activities/04-Stu_Create-Read/Unsolved/server.js
@@ -1,51 +1,69 @@
-const express = require('express');
-const mongodb = require('mongodb').MongoClient;
+// external imports
+const express = require("express");
+const mongodb = require("mongodb").MongoClient;
 
+// declare express server app and port
 const app = express();
 const port = 3001;
 
+// declare database connection URI
 const connectionStringURI = `mongodb://localhost:27017/inventoryDB`;
 
 let db;
 
+// connect to mongodb database
 mongodb.connect(
+  // connection uri
   connectionStringURI,
+  // options
   { useNewUrlParser: true, useUnifiedTopology: true },
   (err, client) => {
     db = client.db();
+
+    // connect to port
     app.listen(port, () => {
       console.log(`Example app listening at http://localhost:${port}`);
     });
   }
 );
 
+// middleware
 app.use(express.json());
 
-app.post('/create', (req, res) => {
-  db.collection('bookCollection').insertOne(
+// /create route
+app.post("/create", (req, res) => {
+  // insert record to 'bookCollection' collection
+  db.collection("bookCollection").insertOne(
+    // pass title and author from req.body
     { title: req.body.title, author: req.body.author },
+
     (err, results) => {
       if (err) throw err;
+
+      // return result if successful
       res.json(results);
     }
   );
 });
 
-app.post('/create-many', function (req, res) {
-  db.collection('bookCollection').insertMany(
-    [
-      {"title" : "Oh the Places We Will Go!"},
-      {"title" : "Diary of Anne Frank"}
-    ], 
-    (err,results) => {
+// /create-many route
+app.post("/create-many", function (req, res) {
+  // insert to "bookCollection" collection 2 documents with titles:
+  db.collection("bookCollection").insertMany(
+    [{ title: "Oh the Places We Will Go!" }, { title: "Diary of Anne Frank" }],
+    (err, results) => {
       if (err) throw err;
+
+      // return results if successful
       res.json(results);
     }
   );
 });
 
-app.get('/read', (req, res) => {
-  db.collection('bookCollection')
+// /read route
+app.get("/read", (req, res) => {
+  // from 'bookCollection' find all documents, send back as array
+  db.collection("bookCollection")
     .find({})
     .toArray((err, results) => {
       if (err) throw err;
